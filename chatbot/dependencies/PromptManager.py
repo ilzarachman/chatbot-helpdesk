b51f56215ca1c5f53285755a4cd5d4d8fca1947c
Ilza Rachman <ilzarachman2@gmail.com>
2024-06-09 16:51:14 +0700

    refactor: PromptManager now could add context dynamically

--- chatbot/dependencies/PromptManager.py
+++ chatbot/dependencies/PromptManager.py
@@ -62,3 +62,53 @@ class PromptManager:
             raise KeyError(f"Prompt '{prompt_name}' not found in '{prompt_filename}' file.")
         except Exception as e:
             raise e
+
+    @classmethod
+    def get_prompt_template(cls, prompt_filename: str, prompt_name: str) -> Template:
+        """
+        A method to get a prompt template based on the prompt filename and prompt name.
+
+        Args:
+            cls: The class reference.
+            prompt_filename (str): The filename of the prompt.
+            prompt_name (str): The name of the prompt.
+
+        Returns:
+            Template: The prompt template.
+
+        Raises:
+            KeyError: If the prompt name is not found in the prompt file.
+        """
+        try:
+            prompt_file = cls._read_prompt_file(prompt_filename)
+            template_string = Template(prompt_file[prompt_name])
+            return template_string
+        except KeyError:
+            raise KeyError(f"Prompt '{prompt_name}' not found in '{prompt_filename}' file.")
+        except Exception as e:
+            raise e
+
+    @classmethod
+    def give_context_to_template(cls, prompt_template: Template, context: dict) -> str:
+        """
+        A method to give context to a prompt template.
+
+        Args:
+            cls: The class reference.
+            prompt_template (Template): The prompt template.
+            context (dict): The context to give to the prompt template.
+
+        Returns:
+            Template: The prompt template with the context.
+
+        Raises:
+            ValueError: If the prompt template is not a Template object.
+        """
+        if not isinstance(prompt_template, Template):
+            raise ValueError("Prompt template must be a Template object.")
+
+        try:
+            return prompt_template.render(**context)
+        except Exception as e:
+            raise e
+
